2b1998bb90ffd8f3ad98b29677cd040cfa8f830c
Paul Brehmer <paulbrehmer8@googlemail.com>
2023-07-15 15:03:19 +0200

    Add rendervideo function, update README

--- examples/export.jl
+++ examples/export.jl
@@ -4,19 +4,24 @@ using Asasine
 using Dates: now
 
 ## KS parameters
-Lx = 16
+Lx = 128
 Nx = 10Lx
 dt = 1 / 32
 x  = Lx / Nx * collect(1:Nx)
-u0 = @. 2sin(2π * x / Lx) * (cos(x) + 0.1 * cos(x / 16) * (1 + 2 * sin(x / 16)))
-# u0 = 12randn(Nx) .* @.(cos(x) + 0.2 * cos(x / 8) * (1 + 2 * sin(x / 4)))
+# u0 = @. 2sin(2π * x / Lx) * (cos(x) + 0.1 * cos(x / 16) * (1 + 2 * sin(x / 16)))
+u0 = 10randn(Nx) .* @.(cos(x) + 0.2 * cos(x / 8) * (1 + 2 * sin(x / 4)))
 # u0 = 3 * (rand(Nx) .- 0.3)
+# div = 2; u0 = vcat([fill((-1.0)^n, Nx ÷ div) for n in 1:div]...)
 ks = KSIntegrator{Float32}(u0, Lx, dt)
 
 # Export static heatmap of entire KS solution
-f, = renderimage(ks, "examples/ks_L$(Lx)_$(now()).png"; Nt=1400, resolution=(1200, 800));
+renderimage(ks, "examples/ks_L$(Lx)_$(now()).png"; Nt=1600, resolution=(1200, 800));
 
-## Audio parameters
+# Export animated heatmap
+rendervideo(ks, "examples/ks_L$(Lx)_$(now()).mp4";
+            Nt=2000, Ntinit=200, scroll=false, fps=60)
+
+# Audio parameters
 fps     = 12Hz
 freqidx = 4:Int(Nx)÷100:Int(Nx)-4
 freqs   = @. 60.0 + 3000.0tanh(0.2 * freqidx / Nx)
@@ -25,5 +30,3 @@ sbuf    = SampleBuf(Float32, 44100, 1 / fps, 2);
 
 # Export audio
 sbuftot = renderaudio(sbuf, son, ks, "examples/ks_L$(Lx)_$(now()).wav");
-
-# TODO: Export animated heatmap
--- src/render.jl
+++ src/render.jl
@@ -31,6 +31,21 @@ function renderimage(ks::KSIntegrator, filename::AbstractString;
     fig, ax, hm
 end
 
-function rendervideo(ks::KSIntegrator; Nt=600, nplot=1, resolution=(1000, 600), theme=kstheme)
+function rendervideo(ks::KSIntegrator, filename::AbstractString;
+                     Nt=1200, Ntinit=600, nplot=1, resolution=(1200, 800), theme=kstheme,
+                     hmkwargs=(; colormap=:hawaii), scroll=false, fps=30)
+    set_theme!(theme)
+    U, t, x = integrate(ks, Nt; nplot)
+    Umax = maximum(abs, U)
 
+    fig, ax, hm = heatmap(t, x, U; hmkwargs..., colorrange=(-Umax, Umax))
+    xlims!(0, Ntinit * ks.dt)
+    hidedecorations!(ax)
+
+    record(fig, filename, Ntinit:nplot:Nt; framerate=fps) do t
+        count = Ntinit - t
+        xlims!(scroll ? count : 0, t * ks.dt)
+    end
+
+    fig, ax, hm
 end
